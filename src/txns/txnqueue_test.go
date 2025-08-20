c188c2152a04c41ed36a70d040fb9ce6a6b95f7b
Danila Slesarev <blackdeer1524@yandex.ru>
2025-08-20 19:01:37 +0300

    more locking features

--- src/txns/txnqueue_test.go
+++ src/txns/txnqueue_test.go
@@ -574,6 +574,7 @@ func assertQueueConsistency[LockModeType GranularLock[LockModeType], ObjectIDTyp
 
 	cur := q.head.next
 	cur.mu.Lock()
+	cur = cur.SafeNext()
 	for ; cur != q.tail; cur = cur.SafeNext() {
 		qLen++
 		txnNodes[cur] = struct{}{}
