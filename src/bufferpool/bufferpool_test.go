bd451a2e9f63f53921cf3d6515aa2e7c3f77c598
Danila Slesarev <blackdeer1524@yandex.ru>
2025-08-23 20:34:15 +0300

    pain

--- src/bufferpool/bufferpool_test.go
+++ src/bufferpool/bufferpool_test.go
@@ -1,13 +1,17 @@
 package bufferpool
 
 import (
+	"fmt"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/Blackdeer1524/GraphDB/src/pkg/common"
+	"github.com/Blackdeer1524/GraphDB/src/storage/disk"
 	"github.com/Blackdeer1524/GraphDB/src/storage/page"
 )
 
@@ -220,4 +224,122 @@ func TestGetPage_LoadFromDisk_WithVictimReplacement(t *testing.T) {
 
 	mockReplacer.AssertExpectations(t)
 	mockDisk.AssertExpectations(t)
+}
+
+type concurrentFakeDisk struct {
+	t       *testing.T
+	mu      sync.Mutex
+	storage map[common.PageIdentity]*page.SlottedPage
+}
+
+func newConcurrentFakeDisk(t *testing.T) *concurrentFakeDisk {
+	return &concurrentFakeDisk{
+		t:       t,
+		storage: make(map[common.PageIdentity]*page.SlottedPage),
+	}
+}
+
+var _ common.DiskManager[*page.SlottedPage] = &concurrentFakeDisk{}
+
+func pageIdentString(pIdent common.PageIdentity) string {
+	return fmt.Sprintf("PAGE:%d:%d", pIdent.FileID, pIdent.PageID)
+}
+
+func ensurePage(t *testing.T, pg *page.SlottedPage, pIdent common.PageIdentity) {
+	pg.RLock()
+	defer pg.RUnlock()
+
+	expected := []byte(pageIdentString(pIdent))
+	if !assert.Equal(t, uint16(1), pg.NumSlots()) {
+		return
+	}
+
+	data := pg.Read(0)
+	assert.Equal(t, expected, data)
+}
+
+func (d *concurrentFakeDisk) GetPageNoNew(pg *page.SlottedPage, pIdent common.PageIdentity) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	storedPage, ok := d.storage[pIdent]
+	if ok {
+		pg.SetData(storedPage.GetData())
+		pg.UnsafeInitLatch()
+		return nil
+	}
+
+	return disk.ErrNoSuchPage
+}
+
+func (d *concurrentFakeDisk) ReadPage(pg *page.SlottedPage, pIdent common.PageIdentity) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	storedPage, ok := d.storage[pIdent]
+	if ok {
+		require.Equal(d.t, uint16(1), storedPage.NumSlots())
+		pg.SetData(storedPage.GetData())
+		pg.UnsafeInitLatch()
+		return nil
+	}
+
+	newPage := page.NewSlottedPage()
+	require.NotNil(d.t, newPage)
+	require.Equal(d.t, uint16(0), newPage.NumSlots())
+	slotOpt := newPage.Insert([]byte(pageIdentString(pIdent)))
+	if slotOpt.IsNone() {
+		return fmt.Errorf("failed to insert page: %+v", pIdent)
+	}
+	require.Equal(d.t, uint16(1), newPage.NumSlots())
+	d.storage[pIdent] = newPage
+	pg.SetData(newPage.GetData())
+	return nil
+}
+
+func (d *concurrentFakeDisk) WritePage(page *page.SlottedPage, pIdent common.PageIdentity) error {
+	return nil
+}
+
+func TestManager_ConcurrentReplacement(t *testing.T) {
+	disk := newConcurrentFakeDisk(t)
+	replacer := NewLRUReplacer()
+
+	const poolSize uint64 = 4
+	const numPages = 32
+	const numWorkers = 4
+	const opsPerWorker = 500
+
+	pool := NewDebugBufferPool(New(poolSize, replacer, disk), map[common.PageIdentity]struct{}{})
+	defer func() { assert.NoError(t, pool.EnsureAllPagesUnpinnedAndUnlocked()) }()
+
+	fileID := common.FileID(1)
+
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+	for w := 0; w < numWorkers; w++ {
+		workerID := w
+		go func() {
+			defer wg.Done()
+			for i := 0; i < opsPerWorker; i++ {
+				pageIdx := (i*7 + workerID*3) % numPages
+				pid := common.PageIdentity{
+					FileID: fileID,
+					PageID: common.PageID(pageIdx),
+				}
+
+				func() {
+					pg, err := pool.GetPage(pid)
+					if !assert.NoError(t, err) {
+						return
+					}
+					defer pool.Unpin(pid)
+
+					ensurePage(t, pg, pid)
+					time.Sleep(100 * time.Microsecond)
+				}()
+			}
+		}()
+	}
+	wg.Wait()
 }
