9927eec2b6e362ea3169de62b7523d2845eb01d1
Alexey Lisov <lisov.a2005@yandex.ru>
2025-08-28 02:30:57 +0300

    some

--- src/storage/index/hash/hash.go
+++ src/storage/index/hash/hash.go
@@ -1,11 +1,14 @@
 package hash
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"github.com/Blackdeer1524/GraphDB/src/pkg/assert"
 	"hash/fnv"
 
 	"github.com/Blackdeer1524/GraphDB/src/pkg/common"
+	"github.com/Blackdeer1524/GraphDB/src/pkg/utils"
 	"github.com/Blackdeer1524/GraphDB/src/storage/page"
 )
 
@@ -16,10 +19,14 @@ const (
 	dirPageBucketsPtrsCount        = 100
 )
 
+var (
+	ErrKeyNotFound = errors.New("key not found")
+)
+
 type StorageEngine interface {
 	GetPage(pageID uint64, fileID uint64) (*page.SlottedPage, error)
 	UnpinPage(pageID uint64, fileID uint64) error
-	WritePage(pageID uint64, fileID uint64, page page.SlottedPage) error
+	WritePage(pageID uint64, fileID uint64, p *page.SlottedPage) error
 }
 
 type Index struct {
@@ -30,10 +37,11 @@ type Index struct {
 	keySize int
 }
 
-func New(indexFileID uint64, se StorageEngine, index int) (*Index, error) {
+func New(indexFileID uint64, se StorageEngine, keySize int) (*Index, error) {
 	return &Index{
-		se:      se,
-		keySize: index,
+		se:          se,
+		indexFileID: indexFileID,
+		keySize:     keySize,
 	}, nil
 }
 
@@ -46,12 +54,8 @@ func hashKeyToUint64(key []byte) uint64 {
 
 type rootPage struct {
 	p              *page.SlottedPage
-	d              uint64
+	d              uint16
 	overflowPageID uint64
-}
-
-type metaPage struct {
-	p *page.SlottedPage
 }
 
 type dirPage struct {
@@ -60,37 +64,160 @@ type dirPage struct {
 
 type bucketPage struct {
 	p *page.SlottedPage
-	l uint64
+	l uint16
 }
 
-func (bp *bucketPage) find(key []byte) (common.RecordID, error) {
-	return common.RecordID{}, nil
+func (bp *bucketPage) find(key []byte) (*common.RecordID, error) {
+	ns := bp.p.NumSlots()
+
+	keyLen := uint16(len(key))
+
+	// FileID + PageID + SlotNum + Existence
+	const ridBytes = 8 + 8 + 2 + 2
+
+	// skip slot with local depth
+	for i := uint16(1); i < ns; i++ {
+		slot := bp.p.Read(i)
+		if uint16(len(slot)) != keyLen+ridBytes {
+			continue
+		}
+
+		if !bytes.Equal(slot[:keyLen], key) {
+			continue
+		}
+
+		base := keyLen
+		fid := utils.FromBytes[uint64](slot[base : base+8])
+		pid := utils.FromBytes[uint64](slot[base+8 : base+16])
+		sn := utils.FromBytes[uint16](slot[base+16 : base+18])
+		ext := utils.FromBytes[uint16](slot[base+18 : base+20])
+
+		if ext == 0 {
+			continue
+		}
+
+		rid := &common.RecordID{
+			FileID:  common.FileID(fid),
+			PageID:  common.PageID(pid),
+			SlotNum: sn,
+		}
+
+		return rid, nil
+	}
+
+	return nil, ErrKeyNotFound
 }
 
 func (h *Index) getRootPage() (*rootPage, error) {
-	panic("not implemented")
+	rp, err := h.se.GetPage(indexRootPageID, h.indexFileID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get root page: %w", err)
+	}
+
+	if rp.NumSlots() < 2 {
+		return nil, errors.New("corrupted root page: not enough header slots")
+	}
+
+	overflowPageID := utils.FromBytes[uint64](rp.Read(0))
+	d := utils.FromBytes[uint16](rp.Read(1))
+
+	return &rootPage{
+		p:              rp,
+		d:              d,
+		overflowPageID: overflowPageID,
+	}, nil
 }
 
-func (h *Index) getMetaPage(rp *rootPage, num uint64) (*metaPage, error) {
-	panic("not implemented")
+func (h *Index) getMetaPage(rp *rootPage, num uint64) (*rootPage, error) {
+	if num == 0 {
+		return rp, nil
+	}
+
+	nextPageID := rp.overflowPageID
+	if nextPageID == overflowPageNotExist {
+		return nil, errors.New("meta page does not exist")
+	}
+
+	var (
+		cur *page.SlottedPage
+		err error
+	)
+
+	for i := uint64(1); i <= num; i++ {
+		cur, err = h.se.GetPage(nextPageID, h.indexFileID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get meta page %d: %w", i, err)
+		}
+
+		if i < num {
+			nextPageID = utils.FromBytes[uint64](cur.Read(0))
+			if nextPageID == overflowPageNotExist {
+				return nil, errors.New("chain of overflow pages ended early")
+			}
+		}
+	}
+
+	assert.Assert(cur != nil, "cur page is nil")
+
+	return &rootPage{
+		p:              cur,
+		overflowPageID: utils.FromBytes[uint64](cur.Read(0)),
+	}, nil
 }
 
-func (h *Index) getDirPage(mp *metaPage, num uint64) (*dirPage, error) {
-	panic("not implemented")
+func (h *Index) getDirPage(rp *rootPage, num uint64) (*dirPage, error) {
+	ns := rp.p.NumSlots()
+
+	// because one slot is for overflow page and second is for global depth
+	if uint64(ns-2) <= num {
+		return nil, errors.New("dir page not exist")
+	}
+
+	dirPageID := utils.FromBytes[uint64](rp.p.Read(uint16(num + 2)))
+
+	dp, err := h.se.GetPage(dirPageID, h.indexFileID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get dir page: %w", err)
+	}
+
+	return &dirPage{
+		p: dp,
+	}, nil
 }
 
-func (h *Index) getBucketPage(dp *dirPage, num uint64) (*bucketPage, error) {
-	panic("not implemented")
+func (h *Index) getBucketPage(dp *dirPage, num uint16) (*bucketPage, error) {
+	ns := dp.p.NumSlots()
+
+	if ns <= num {
+		return nil, errors.New("bucket page not exist")
+	}
+
+	slot := dp.p.Read(num)
+	if len(slot) < 8 {
+		return nil, errors.New("corrupted dir page slot: too small for pageID")
+	}
+
+	bucketPageID := utils.FromBytes[uint64](slot)
+
+	bp, err := h.se.GetPage(bucketPageID, h.indexFileID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get bucket page: %w", err)
+	}
+
+	return &bucketPage{
+		p: bp,
+		l: utils.FromBytes[uint16](bp.Read(0)),
+	}, nil
 }
 
-func (h *Index) Get(key []byte) (common.RecordID, error) {
+func (h *Index) Get(key []byte) (*common.RecordID, error) {
 	if len(key) != h.keySize {
-		return common.RecordID{}, errors.New("key size is not equal to index key size")
+		return nil, errors.New("key size is not equal to index key size")
 	}
 
 	rp, err := h.getRootPage()
 	if err != nil {
-		return common.RecordID{}, fmt.Errorf("failed to get root page: %w", err)
+		return nil, fmt.Errorf("failed to get root page: %w", err)
 	}
 
 	keyHash := hashKeyToUint64(key)
@@ -102,27 +229,27 @@ func (h *Index) Get(key []byte) (common.RecordID, error) {
 
 	mp, err := h.getMetaPage(rp, mpIndex)
 	if err != nil {
-		return common.RecordID{}, fmt.Errorf("failed to get meta page: %w", err)
+		return nil, fmt.Errorf("failed to get meta page: %w", err)
 	}
 
 	mpOffset := index % (metaPageDirPagesCount * dirPageBucketsPtrsCount)
 
 	dirSlot := mpOffset / dirPageBucketsPtrsCount
-	bucketSlot := mpOffset % dirPageBucketsPtrsCount
+	bucketSlot := uint16(mpOffset % dirPageBucketsPtrsCount)
 
 	dp, err := h.getDirPage(mp, dirSlot)
 	if err != nil {
-		return common.RecordID{}, fmt.Errorf("failed to get dir page: %w", err)
+		return nil, fmt.Errorf("failed to get dir page: %w", err)
 	}
 
 	bucketPtr, err := h.getBucketPage(dp, bucketSlot)
 	if err != nil {
-		return common.RecordID{}, fmt.Errorf("failed to get bucket ptr: %w", err)
+		return nil, fmt.Errorf("failed to get bucket ptr: %w", err)
 	}
 
 	rid, err := bucketPtr.find(key)
 	if err != nil {
-		return common.RecordID{}, fmt.Errorf("failed to find key: %w", err)
+		return nil, fmt.Errorf("failed to find key: %w", err)
 	}
 
 	return rid, nil
---
+++ src/storage/index/hash/hash_test.go
@@ -0,0 +1,163 @@
+package hash
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Blackdeer1524/GraphDB/src/pkg/common"
+	"github.com/Blackdeer1524/GraphDB/src/pkg/utils"
+	"github.com/Blackdeer1524/GraphDB/src/storage/page"
+)
+
+func makeSlot(key []byte, rid *common.RecordID, exist uint16, keySize int) []byte {
+	b := make([]byte, 0, keySize+20)
+	b = append(b, key...)
+	b = append(b, utils.ToBytes(uint64(rid.FileID))...)
+	b = append(b, utils.ToBytes(uint64(rid.PageID))...)
+	b = append(b, utils.ToBytes(rid.SlotNum)...)
+	b = append(b, utils.ToBytes(exist)...)
+
+	return b
+}
+
+func TestBucketPage_Find(t *testing.T) {
+	const keySize = 4
+	key1 := []byte{1, 2, 3, 4}
+	key2 := []byte{9, 9, 9, 9}
+
+	rid1 := &common.RecordID{
+		FileID:  10,
+		PageID:  20,
+		SlotNum: 30,
+	}
+
+	p := page.NewSlottedPage()
+
+	p.Insert(utils.ToBytes(uint16(1)))
+
+	p.Insert(makeSlot(key1, rid1, 1, keySize))
+
+	p.Insert([]byte{42})
+
+	p.Insert(makeSlot(key2, rid1, 0, keySize))
+
+	bp := &bucketPage{p: p}
+
+	t.Run("found", func(t *testing.T) {
+		got, err := bp.find(key1)
+		require.NoError(t, err)
+		require.Equal(t, rid1, got)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		_, err := bp.find([]byte{7, 7, 7, 7})
+		require.ErrorIs(t, err, ErrKeyNotFound)
+	})
+
+	t.Run("deleted record ignored", func(t *testing.T) {
+		_, err := bp.find(key2)
+		require.ErrorIs(t, err, ErrKeyNotFound)
+	})
+}
+
+type mockStorageEngine struct {
+	pages map[uint64]*page.SlottedPage
+	err   error
+}
+
+func (m *mockStorageEngine) GetPage(pageID uint64, fileID uint64) (*page.SlottedPage, error) {
+	if m.err != nil {
+		return nil, m.err
+	}
+	p, ok := m.pages[pageID]
+	if !ok {
+		return nil, errors.New("page not found")
+	}
+	return p, nil
+}
+func (m *mockStorageEngine) UnpinPage(pageID uint64, fileID uint64) error { return nil }
+
+func (m *mockStorageEngine) WritePage(pageID uint64, fileID uint64, p *page.SlottedPage) error {
+	m.pages[pageID] = p
+
+	return nil
+}
+
+func TestIndex_Get_KeySizeMismatch(t *testing.T) {
+	se := &mockStorageEngine{}
+	idx, _ := New(1, se, 4)
+
+	_, err := idx.Get([]byte{1, 2}) // неверная длина
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "key size is not equal")
+}
+
+func TestIndex_Get_Success(t *testing.T) {
+	const keySize = 4
+	key := []byte{1, 2, 3, 4}
+	rid := &common.RecordID{FileID: 5, PageID: 6, SlotNum: 7}
+
+	root := page.NewSlottedPage()
+	root.Insert(utils.ToBytes(overflowPageNotExist))
+	root.Insert(utils.ToBytes(uint16(1)))
+	root.Insert(utils.ToBytes(uint64(10)))
+
+	dir := page.NewSlottedPage()
+	dir.Insert(utils.ToBytes(uint64(18)))
+	dir.Insert(utils.ToBytes(uint64(20)))
+
+	// bucket page
+	bucket := page.NewSlottedPage()
+	bucket.Insert(utils.ToBytes(uint16(1)))
+	bucket.Insert(makeSlot(key, rid, 1, keySize))
+
+	se := &mockStorageEngine{
+		pages: map[uint64]*page.SlottedPage{
+			0:  root,
+			10: dir,
+			20: bucket,
+		},
+	}
+
+	idx, err := New(1, se, keySize)
+	require.NoError(t, err)
+
+	got, err := idx.Get(key)
+	require.NoError(t, err)
+	require.Equal(t, rid, got)
+}
+
+func TestIndex_Get_KeyNotFound(t *testing.T) {
+	const keySize = 4
+	key := []byte{1, 2, 3, 4}
+
+	root := page.NewSlottedPage()
+	root.Insert(utils.ToBytes(uint64(overflowPageNotExist)))
+	root.Insert(utils.ToBytes(uint16(1)))
+	root.Insert(utils.ToBytes(uint64(10)))
+
+	dir := page.NewSlottedPage()
+	dir.Insert(utils.ToBytes(uint64(18)))
+	dir.Insert(utils.ToBytes(uint64(20)))
+
+	bucket := page.NewSlottedPage()
+	bucket.Insert(utils.ToBytes(uint16(1)))
+
+	se := &mockStorageEngine{
+		pages: map[uint64]*page.SlottedPage{
+			0:  root,
+			10: dir,
+			20: bucket,
+		},
+	}
+
+	idx, err := New(1, se, keySize)
+	require.NoError(t, err)
+
+	_, err = idx.Get(key)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to find key")
+}
