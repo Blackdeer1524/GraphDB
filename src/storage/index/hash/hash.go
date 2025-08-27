3df25174afcb2745062457d4d06d3c8bf2bf683d
Alexey Lisov <lisov.a2005@yandex.ru>
2025-08-27 03:47:24 +0300

    some

--- src/storage/index/hash/hash.go
+++ src/storage/index/hash/hash.go
@@ -1,20 +1,20 @@
 package hash
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
+	"hash/fnv"
 
 	"github.com/Blackdeer1524/GraphDB/src/pkg/common"
 	"github.com/Blackdeer1524/GraphDB/src/storage/page"
 )
 
 const (
-	indexRootPageID uint64 = 0
+	indexRootPageID         uint64 = 0
+	overflowPageNotExist           = ^uint64(0)
+	metaPageDirPagesCount          = 100
+	dirPageBucketsPtrsCount        = 100
 )
-
-type HashIndex struct {
-}
 
 type StorageEngine interface {
 	GetPage(pageID uint64, fileID uint64) (*page.SlottedPage, error)
@@ -37,54 +37,101 @@ func New(indexFileID uint64, se StorageEngine, index int) (*Index, error) {
 	}, nil
 }
 
+func hashKeyToUint64(key []byte) uint64 {
+	hasher := fnv.New64a()
+	_, _ = hasher.Write(key)
+
+	return hasher.Sum64()
+}
+
+type rootPage struct {
+	p              *page.SlottedPage
+	d              uint64
+	overflowPageID uint64
+}
+
+type metaPage struct {
+	p *page.SlottedPage
+}
+
+type dirPage struct {
+	p *page.SlottedPage
+}
+
+type bucketPage struct {
+	p *page.SlottedPage
+	l uint64
+}
+
+func (bp *bucketPage) find(key []byte) (common.RecordID, error) {
+	return common.RecordID{}, nil
+}
+
+func (h *Index) getRootPage() (*rootPage, error) {
+	panic("not implemented")
+}
+
+func (h *Index) getMetaPage(rp *rootPage, num uint64) (*metaPage, error) {
+	panic("not implemented")
+}
+
+func (h *Index) getDirPage(mp *metaPage, num uint64) (*dirPage, error) {
+	panic("not implemented")
+}
+
+func (h *Index) getBucketPage(dp *dirPage, num uint64) (*bucketPage, error) {
+	panic("not implemented")
+}
+
 func (h *Index) Get(key []byte) (common.RecordID, error) {
 	if len(key) != h.keySize {
 		return common.RecordID{}, errors.New("key size is not equal to index key size")
 	}
 
-	// 1. Получаем root page
-	rootPage, err := h.se.GetPage(indexRootPageID, h.indexFileID)
+	rp, err := h.getRootPage()
 	if err != nil {
 		return common.RecordID{}, fmt.Errorf("failed to get root page: %w", err)
 	}
-	defer func() {
-		_ = h.se.UnpinPage(indexRootPageID, h.indexFileID)
-	}()
 
-	root := decodeRootPage(rootPage.GetData())
-	hashVal := h.hashKey(key)
+	keyHash := hashKeyToUint64(key)
 
-	// 3. Берём d младших бит
-	dirIdx := hashVal & ((1 << root.GlobalDepth) - 1)
+	index := keyHash & ((1 << rp.d) - 1)
 
-	dirPageID := root.DirectoryPointers[dirIdx/root.DirPageFanout]
-	offset := dirIdx % root.DirPageFanout
+	// calc an index of meta page
+	mpIndex := index / (metaPageDirPagesCount * dirPageBucketsPtrsCount)
 
-	dirPage, err := h.se.GetPage(dirPageID, h.indexFileID)
+	mp, err := h.getMetaPage(rp, mpIndex)
 	if err != nil {
-		return common.RecordID{}, fmt.Errorf("failed to get directory page: %w", err)
+		return common.RecordID{}, fmt.Errorf("failed to get meta page: %w", err)
 	}
-	defer func() {
-		_ = h.se.UnpinPage(dirPageID, h.indexFileID)
-	}()
 
-	directory := decodeDirectoryPage(dirPage.Data())
-	bucketPageID := directory.BucketPointers[offset]
+	mpOffset := index % (metaPageDirPagesCount * dirPageBucketsPtrsCount)
 
-	bucketPage, err := h.se.GetPage(bucketPageID, h.indexFileID)
+	dirSlot := mpOffset / dirPageBucketsPtrsCount
+	bucketSlot := mpOffset % dirPageBucketsPtrsCount
+
+	dp, err := h.getDirPage(mp, dirSlot)
 	if err != nil {
-		return common.RecordID{}, fmt.Errorf("failed to get bucket page: %w", err)
-	}
-	defer func() {
-		_ = h.se.UnpinPage(bucketPageID, h.indexFileID)
-	}()
-
-	bucket := decodeBucketPage(bucketPage.Data())
-	for _, entry := range bucket.Entries {
-		if bytes.Equal(entry.Key, key) {
-			return entry.RID, nil
-		}
+		return common.RecordID{}, fmt.Errorf("failed to get dir page: %w", err)
 	}
 
-	return common.RecordID{}, errors.New("key not found")
+	bucketPtr, err := h.getBucketPage(dp, bucketSlot)
+	if err != nil {
+		return common.RecordID{}, fmt.Errorf("failed to get bucket ptr: %w", err)
+	}
+
+	rid, err := bucketPtr.find(key)
+	if err != nil {
+		return common.RecordID{}, fmt.Errorf("failed to find key: %w", err)
+	}
+
+	return rid, nil
+}
+
+func (h *Index) Delete(key []byte) (common.RecordID, error) {
+	panic("not implemented")
+}
+
+func (h *Index) Insert(key []byte, rid common.RecordID) (common.RecordID, error) {
+	panic("not implemented")
 }
