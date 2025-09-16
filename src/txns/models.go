bedcb43a9fdd975dd7166b50e4c148edaaeb4691
Danila Slesarev <blackdeer1524@yandex.ru>
2025-09-17 02:15:03 +0300

    rename

--- src/txns/models.go
+++ src/txns/models.go
@@ -12,7 +12,7 @@ type TaggedType[T any] struct{ v T } // this trick forbids casting one lock mode
 type SimpleLockMode TaggedType[uint8]
 type GranularLockMode TaggedType[uint16]
 
-type GranularLock[Lock any] interface {
+type DatabaseLock[Lock any] interface {
 	fmt.Stringer
 	Compatible(Lock) bool
 	Combine(Lock) Lock
@@ -33,8 +33,8 @@ var (
 )
 
 var (
-	_ GranularLock[SimpleLockMode]   = SimpleLockMode{0}
-	_ GranularLock[GranularLockMode] = GranularLockMode{0}
+	_ DatabaseLock[SimpleLockMode]   = SimpleLockMode{0}
+	_ DatabaseLock[GranularLockMode] = GranularLockMode{0}
 )
 
 func (m SimpleLockMode) String() string {
@@ -312,13 +312,13 @@ func (m GranularLockMode) WeakerOrEqual(other GranularLockMode) bool {
 	panic("unreachable")
 }
 
-type TxnLockRequest[LockModeType GranularLock[LockModeType], ObjectIDType comparable] struct {
+type TxnLockRequest[LockModeType DatabaseLock[LockModeType], ObjectIDType comparable] struct {
 	txnID    common.TxnID
 	objectId ObjectIDType
 	lockMode LockModeType
 }
 
-func NewTxnLockRequest[LockModeType GranularLock[LockModeType], ObjectIDType comparable](
+func NewTxnLockRequest[LockModeType DatabaseLock[LockModeType], ObjectIDType comparable](
 	txnID common.TxnID,
 	objectId ObjectIDType,
 	lockMode LockModeType,
